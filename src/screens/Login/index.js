c60ad958dcd02c84fa2675d986dd46b0d7880743
eduardo-moro <eduardomoro1127@gmail.com>
2021-02-21 01:20:52 -0300

    update

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import { View, Text, StyleSheet } from 'react-native'; 
 
-export default () => {
+export default ( props ) => {
     return(
         <View style={styles.container}>
             <Text style={styles.text}>Login Screen</Text>
--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -26,7 +26,7 @@ const styles = StyleSheet.create(
     {
     container:{
         flex:1, 
-        justifyContent:'center', 
+        justifyContent:'space-around', 
         alignItems:'center', 
         backgroundColor:'#ffffff'
     }, 
